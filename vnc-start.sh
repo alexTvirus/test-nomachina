0ff3b1746de6e48be9366e41840ba3f5ead43890
Alex <lisatthu35@gmail.com>
2025-03-19 17:39:31 +0700

    Update vnc-start.sh

--- vnc-start.sh
+++ vnc-start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 whoami
-sudo vncserver -passwd /home/user/.vnc/passwd :1  && sudo websockify --web /usr/share/novnc/ 6080 0.0.0.0:5901
+ vncserver -passwd /home/user/.vnc/passwd :1  &&  websockify --web /usr/share/novnc/ 6080 0.0.0.0:5901
