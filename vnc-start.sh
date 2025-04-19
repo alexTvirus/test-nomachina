d142b4ec0c36b1b7523f9d09d951e1b1f4d1d0c3
Alex <lisatthu35@gmail.com>
2025-04-19 14:57:32 +0700

    Update vnc-start.sh

--- vnc-start.sh
+++ vnc-start.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 whoami
-vncserver -passwd /home/user/.vnc/passwd :1  && websockify --web /usr/share/novnc/ 6080 127.0.0.1:5901
+
+# cài đặt nodejs
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf '%s/.nvm' "${HOME}" || printf '%s/nvm' "${XDG_CONFIG_HOME}")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash completion
+nvm install 14
+nvm use 14
+
+# --------------------- test voi lite-tunel seft host
+
+git clone https://github.com/alexTvirus/wstunel-client.git
+
+vncserver -passwd /home/user/.vnc/passwd :1  &
+
+bash /home/seluser/wstunel-client/wstunnel -s 0.0.0.0:7860 -t 127.0.0.1:5901
+
+# -------------------
+
+# websockify --web /usr/share/novnc/ 6080 127.0.0.1:5901 &
